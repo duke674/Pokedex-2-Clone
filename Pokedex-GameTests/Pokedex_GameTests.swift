02a1f1b813d12999c144e7280d91ef4123659752
earl <mkvudu20@live.com>
2016-12-03 23:07:42 -0700

    Initial Commit

---
+++ Pokedex-GameTests/Pokedex_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pokedex_GameTests.swift
+//  Pokedex-GameTests
+//
+//  Created by earl on 12/3/16.
+//  Copyright © 2016 Earl Elmore. All rights reserved.
+//
+
+import XCTest
+@testable import Pokedex_Game
+
+class Pokedex_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
